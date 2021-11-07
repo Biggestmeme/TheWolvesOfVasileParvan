b1b62635ac6815b97613fc3b1cd8d3aabf38fca2
AndreiMariusZed <62403550+AndreiMariusZed@users.noreply.github.com>
2021-11-07 15:51:46 +0200

    Added partial design for Login Page

---
+++ Client/src/main/java/com/bursierii/client/Main.java
@@ -0,0 +1,33 @@
+package com.bursierii.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import org.json.*;
+
+import com.bursierii.client.*;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("The Wolves of Vasile Parvan Street");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
