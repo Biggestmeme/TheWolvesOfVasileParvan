3fafff385250407bfaaa42d493b6f57c922f5901
AndreiMariusZed <62403550+AndreiMariusZed@users.noreply.github.com>
2021-11-28 19:24:13 +0200

    Dashboard GUI start

---
+++ Client/src/main/java/com/bursierii/client/controllers/DashboardController.java
@@ -0,0 +1,12 @@
+package com.bursierii.client.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DashboardController {
+    @FXML
+    public void goToStockDetail(ActionEvent event){
+        System.out.println("Merge");
+    }
+
+}
