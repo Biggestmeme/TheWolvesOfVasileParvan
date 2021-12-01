7538ce4058303f652d58786fbbfdb76450091cd4
felix24mihai <mihai_prava@yahoo.com>
2021-12-01 21:33:11 +0200

    Add user profile page

---
+++ Client/src/main/java/com/bursierii/client/controllers/AccountController.java
@@ -0,0 +1,17 @@
+package com.bursierii.client.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+
+public class AccountController {
+    private Button updateProfileButton, backButton;
+
+    @FXML
+    public void updateProfile(ActionEvent event) {
+        String id = ((Control)event.getSource()).getId();
+        System.out.println(id);
+        // check the filled text fields
+    }
+}
